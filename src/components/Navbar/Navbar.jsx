31cbdc041d0e7a490cd4905797cadc88236e4d2e
Enrique1311 <ejspinelli131176@gmail.com>
2023-11-09 00:24:50 -0300

    The navigation bar was finished and started with the header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { RiMenu2Line, RiCloseLine } from "react-icons/ri";
 import world_logo from "../../assets/world-logo.png";
 import TransparentBtn from "../MyButtons/TransparentBtn/Transparent.jsx/TransparentBtn";
 import OrangeBtn from "../MyButtons/OrangeBtn/OrangeBtn";
 
+const Menu = () => (
+	<div className="navbar-menu">
+		<div className="navbar-links-container">
+			<a href="#home">Home</a>
+			<a href="#whatIsGTP">What is GTP?</a>
+			<a href="#openAI">Open AI</a>
+			<a href="#caseStudies">Case Studies</a>
+			<a href="#library">Library</a>
+		</div>
+		<div className="navbar-sign">
+			{" "}
+			<TransparentBtn>Sign In</TransparentBtn>
+			<OrangeBtn>Sign Up</OrangeBtn>
+		</div>
+	</div>
+);
+
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
 	return (
 		<div className="navbar">
 			<a
@@ -16,18 +35,27 @@ const Navbar = () => {
 					src={world_logo}
 					alt="logo"
 				/>
-				<h3>GTP-3</h3>
+				<h3>GTP3</h3>
 			</a>
-			<div className="navbar-links">
-				<a href="#home">Home</a>
-				<a href="#whatIsGTP">What is GTP?</a>
-				<a href="#openAIc">Open AI</a>
-				<a href="#aseStudies">Case Studies</a>
-				<a href="#library">Library</a>
-			</div>
-			<div className="navbar-sign">
-				<TransparentBtn>Sign In</TransparentBtn>
-				<OrangeBtn>Sign Up</OrangeBtn>
+			<div className="navbar-desk-links">
+				<Menu />
+			</div>{" "}
+			<div className="navbar-mobile-menu">
+				<RiMenu2Line
+					color="white"
+					size={30}
+					onClick={() => setToggleMenu(true)}
+				/>{" "}
+				{toggleMenu && (
+					<div className="navbar-mobile-links scale-up-center">
+						<RiCloseLine
+							color="white"
+							size={30}
+							onClick={() => setToggleMenu(false)}
+						/>
+						<Menu />
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,37 @@
 import React from "react";
-import "./Footer.css";
+import "./Header.css";
+import robot from "../../assets/robot.png";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div
+			className="header section__padding"
+			id="home"
+		>
+			<div className="header-content">
+				{" "}
+				<h1 className="gradient__text">
+					Let's Build Something Amazing With GTP-3 OpenAI
+				</h1>
+				<p className="header-text">
+					Yet bed any for travelling assistance indulgence unpleasing. Not
+					thoughts all exercise blessing. Indulgence way everything joy
+					alteration boisterous the attachment. Party we years to order allow
+					asked of.
+				</p>
+				<div className="header-input">
+					<input
+						tupe="email"
+						placeholder="Your Email Address"
+					/>
+					<button type="button">Get Started</button>
+				</div>
+			</div>
+			<div className="header-image">
+				<img src={robot} />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
